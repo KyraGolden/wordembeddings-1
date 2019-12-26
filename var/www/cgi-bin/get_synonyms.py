3c15ab219ad2df515a2214117db954ad2cdb5e41
salwil <salome.wildermuth@gmail.com>
2019-12-26 09:12:08 +0100

    functionality nr of synonyms

--- var/www/cgi-bin/get_synonyms.py
+++ var/www/cgi-bin/get_synonyms.py
@@ -3,6 +3,7 @@
 import cgi
 import cgitb
 import io
+import json
 import gzip
 from gensim.models.fasttext import FastText
 from sklearn import metrics
@@ -12,18 +13,21 @@ cgitb.enable(display=0, logdir="/logs")
 
 def application(environ, start_response):
     params = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
-    word = params["word"].value
+    word = params["s"].value
     nr =  params["nr"].value
-    synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/short.cc.de.300.vec", word)
+    synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/short.cc.de.300.vec", word, int(nr))
+    output = {}
+    output["syn1"] = str(synonym)
+    output_js = json.dumps(output)
     output = "<h2> Here are " + nr + " synonyms for " + word + ":</h2><br><p>" + synonym + "</p>"
     status = "200 OK"
-    output = output.encode()
-    response_headers = [("Content-type", "text/html; charset=UTF-8"),
-                        ("Content-Length", str(len(output)))]
+    output_js = output_js.encode()
+    response_headers = [("Content-type", "application/json; charset=UTF-8"),
+                        ("Content-Length", str(len(output_js)))]
     start_response(status, response_headers)
-    return [output]
+    return [output_js]
 
-def find_synonyms(fname, word):
+def find_synonyms(fname, word, anzahl):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, d = map(int, fin.readline().split())
     data = {}
@@ -37,6 +41,7 @@ def find_synonyms(fname, word):
         data[tokens[0]] = tokens[1:]
         counter += 1
     distances = {}
+    out_list = []
     for item in data:
         if item == word:
             pass
@@ -46,10 +51,17 @@ def find_synonyms(fname, word):
             except KeyError:
                 #print("Dieses Wort befindet sich nicht in der Datenbank")
                 return "Dieses Wort befindet sich nicht in der Datenbank"
-    key_min = min(distances.keys(), key=(lambda k: distances[k]))
-    print('Min. Distanz')
-    print(key_min)
-    return(key_min)
+    ctr = 0
+    while ctr < anzahl:
+        key_min = min(distances.keys(), key=(lambda k: distances[k]))
+        out_list.append(key_min)
+        del distances[key_min]
+        #print('Min. Distanz')
+        #print(key_min)
+        ctr += 1
+    seperator = ', '
+    return seperator.join(out_list)
+    #return(key_min)
 
 if __name__ == "__main__":
     find_synonyms("../../../cc.de.300.vec", 'Hilferuf')
