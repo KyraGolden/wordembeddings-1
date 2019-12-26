fbd686ac32a75ef867ff3841dfbec11af612c98f
salwil <salome.wildermuth@gmail.com>
2019-12-26 18:39:50 +0100

    neue Funktionalitaeten: Dropdown Anzahl, Radiobutton long- und quickSearch

--- var/www/cgi-bin/get_synonyms.py
+++ var/www/cgi-bin/get_synonyms.py
@@ -4,18 +4,22 @@ import cgi
 import cgitb
 import io
 import json
+import pickle
 import gzip
-from gensim.models.fasttext import FastText
+#from gensim.models.fasttext import FastText
 from sklearn import metrics
 import numpy as np
 cgitb.enable(display=0, logdir="/logs")
-
 
 def application(environ, start_response):
     params = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
     word = params["w"].value
     nr =  params["nr"].value
-    synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/short.cc.de.300.vec", word, int(nr))
+    searchType = params["st"].value
+    if searchType == 'bigSearch':
+        synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/50000.cc.de.300.vec", word, int(nr))
+    else:
+        synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/10000.cc.de.300.vec", word, int(nr))
     output = {}
     output["syn1"] = str(synonym)
     output_js = json.dumps(output)
@@ -31,15 +35,17 @@ def find_synonyms(fname, word, anzahl):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, d = map(int, fin.readline().split())
     data = {}
-    #for line in fin:
-    counter = 0
-    #counter-Limit kann rauf-/runtergesetzt werden (damit Abfragen nicht so lange dauern), im File gibt es aber nur
-    #1000 Einträge.
-    while counter < 999:
-        line = fin.readline()
+    for line in fin:
+        # line = fin.readline()
         tokens = str(line).split(' ')
         data[tokens[0]] = tokens[1:]
-        counter += 1
+    #word_vecs = open('long_word_vecs.pkl', 'rb')
+    #data = pickle.load(word_vecs)
+        #with open('word_vecs.pkl', 'rb') as word_vecs:
+        #    data = pickle.load(word_vecs)
+    # die pickle-Datei sollte eigentlich schon zur Verfügung stehen, sollte das nicht der Fall sein, wird sie hier gleich
+    # erstellt für den nächsten Aufruf.
+
     distances = {}
     out_list = []
     for item in data:
@@ -51,17 +57,18 @@ def find_synonyms(fname, word, anzahl):
             except KeyError:
                 #print("Dieses Wort befindet sich nicht in der Datenbank")
                 return "Dieses Wort befindet sich nicht in der Datenbank"
+            except ValueError:
+                pass
     ctr = 0
     while ctr < anzahl:
         key_min = min(distances.keys(), key=(lambda k: distances[k]))
         out_list.append(key_min)
         del distances[key_min]
-        #print('Min. Distanz')
-        #print(key_min)
         ctr += 1
     seperator = ', '
+    print(seperator.join(out_list))
     return seperator.join(out_list)
     #return(key_min)
 
 if __name__ == "__main__":
-    find_synonyms("../../../cc.de.300.vec", 'Hilferuf')
+    find_synonyms("../../../short.cc.de.300.vec", 'Ehefrau', 2)
