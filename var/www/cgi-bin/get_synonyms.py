8f084f7f6ba426c0d6372adc3fe20f7d47f882a0
salwil <45465496+salwil@users.noreply.github.com>
2020-01-12 19:56:16 +0100

    Add files via upload

--- var/www/cgi-bin/get_synonyms.py
+++ var/www/cgi-bin/get_synonyms.py
@@ -4,6 +4,7 @@ import cgi
 import cgitb
 import io
 import json
+import re
 import pickle
 import gzip
 #from gensim.models.fasttext import FastText
@@ -13,17 +14,16 @@ cgitb.enable(display=0, logdir="/logs")
 
 def application(environ, start_response):
     params = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
-    word = params["w"].value
-    nr =  params["nr"].value
+    word_orig = params["w"].value
+    word_lower = word_orig.lower()
+    sprache_wort = params["sprache_wort"].value
+    nr = params["nr"].value
+    sprache_wordemb = params["sprache_wordemb"].value
     searchType = params["st"].value
-    if searchType == 'bigSearch':
-        synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/50000.cc.de.300.vec", word, int(nr))
-    else:
-        synonym = find_synonyms("/home/pcl3_03/public_html/wordembeddings/10000.cc.de.300.vec", word, int(nr))
+    synonym = find_synonyms(searchType, sprache_wordemb, sprache_wort, word_orig, word_lower, int(nr))
     output = {}
-    output["syn1"] = str(synonym)
+    output["syn1"] = synonym
     output_js = json.dumps(output)
-    output = "<h2> Here are " + nr + " synonyms for " + word + ":</h2><br><p>" + synonym + "</p>"
     status = "200 OK"
     output_js = output_js.encode()
     response_headers = [("Content-type", "application/json; charset=UTF-8"),
@@ -31,44 +31,111 @@ def application(environ, start_response):
     start_response(status, response_headers)
     return [output_js]
 
-def find_synonyms(fname, word, anzahl):
-    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    n, d = map(int, fin.readline().split())
-    data = {}
-    for line in fin:
-        # line = fin.readline()
-        tokens = str(line).split(' ')
-        data[tokens[0]] = tokens[1:]
-    #word_vecs = open('long_word_vecs.pkl', 'rb')
-    #data = pickle.load(word_vecs)
-        #with open('word_vecs.pkl', 'rb') as word_vecs:
-        #    data = pickle.load(word_vecs)
-    # die pickle-Datei sollte eigentlich schon zur Verfügung stehen, sollte das nicht der Fall sein, wird sie hier gleich
-    # erstellt für den nächsten Aufruf.
-
-    distances = {}
-    out_list = []
-    for item in data:
-        if item == word:
-            pass
+def find_synonyms(searchType, sprache_wordemb, sprache_wort, word_orig, word_lower, anzahl):
+    if searchType == 'bigSearch' and sprache_wort == 'd':
+        if sprache_wort == sprache_wordemb:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/10000.cc.de.300.vec"
         else:
-            try:
-                distances[item] = metrics.pairwise.cosine_distances([data[item]], [data[word]])
-            except KeyError:
-                #print("Dieses Wort befindet sich nicht in der Datenbank")
-                return "Dieses Wort befindet sich nicht in der Datenbank"
-            except ValueError:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/10000.wiki.de.align.vec"
+    elif searchType == 'bigSearch':
+        if sprache_wort == sprache_wordemb:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/10000.cc.en.300.vec"
+        else:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/10000.wiki.en.align.vec"
+    elif searchType == 'quickSearch' and sprache_wort == 'd':
+        if sprache_wort == sprache_wordemb:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/5000.cc.de.300.vec"
+        else:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/5000.wiki.de.align.vec"
+    else:
+        if sprache_wort == sprache_wordemb:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/5000.cc.en.300.vec"
+        else:
+            fname_w = "/home/pcl3_03/public_html/wordembeddings/5000.wiki.en.align.vec"
+    if sprache_wort == sprache_wordemb:
+        fin = io.open(fname_w, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        data = {}
+        for line in fin:
+            # line = fin.readline()
+            tokens = str(line).split(' ')
+            data[tokens[0]] = tokens[1:]
+        distances = {}
+        out_list = []
+        for item in data:
+            if item == word_orig:
                 pass
-    ctr = 0
-    while ctr < anzahl:
-        key_min = min(distances.keys(), key=(lambda k: distances[k]))
-        out_list.append(key_min)
-        del distances[key_min]
-        ctr += 1
-    seperator = ', '
-    print(seperator.join(out_list))
-    return seperator.join(out_list)
-    #return(key_min)
+            else:
+                try:
+                    distances[item] = metrics.pairwise.cosine_distances([data[item]], [data[word_orig]])
+                except KeyError:
+                    return "Dieses Wort befindet sich nicht in der Sammlung."
+                except ValueError:
+                    pass
+        ctr = 0
+        while ctr < anzahl:
+            key_min = min(distances.keys(), key=(lambda k: distances[k]))
+            out_list.append(key_min)
+            del distances[key_min]
+            ctr += 1
+        seperator = ', '
+        return seperator.join(out_list)
+        #return(key_min)
+    else:
+        if searchType == 'bigSearch' and sprache_wordemb == 'd':
+            #fname_we = "/home/pcl3_03/public_html/wordembeddings/50000.cc.de.300.vec"
+            fname_we = "/home/pcl3_03/public_html/wordembeddings/10000.wiki.de.align.vec"
+        elif searchType == 'bigSearch':
+            #fname_we = "/home/pcl3_03/public_html/wordembeddings/50000.cc.en.300.vec"
+            fname_we = "/home/pcl3_03/public_html/wordembeddings/10000.wiki.en.align.vec"
+        elif searchType == 'quickSearch' and sprache_wordemb == 'd':
+            #fname_we = "/home/pcl3_03/public_html/wordembeddings/10000.cc.de.300.vec"
+            fname_we = "/home/pcl3_03/public_html/wordembeddings/5000.wiki.de.align.vec"
+        else:
+            #fname_we = "/home/pcl3_03/public_html/wordembeddings/10000.cc.en.300.vec"
+            fname_we = "/home/pcl3_03/public_html/wordembeddings/5000.wiki.en.align.vec"
+        fin_w = io.open(fname_w, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        data_w = {}
+        word_found = False
+        for line in fin_w:
+            # line = fin.readline()
+            tokens = str(line).split(' ')
+            data_w[tokens[0]] = tokens[1:]
+            if tokens[0] == word_lower:
+                word_vec = data_w[tokens[0]]
+                word_found = True
+                break
+        if word_found:
+            fin_we = io.open(fname_we, 'r', encoding='utf-8', newline='\n', errors='ignore')
+            data_we = {}
+            for line in fin_we:
+                # line = fin.readline()
+                tokens = str(line).split(' ')
+                data_we[tokens[0]] = tokens[1:]
+            distances = {}
+            out_list = []
+            for item in data_we:
+                try:
+                    distances[item] = metrics.pairwise.cosine_distances([data_we[item]], [data_w[word_lower]])
+                except ValueError:
+                    pass
+            ctr = 0
+            while ctr < anzahl:
+                key_min = min(distances.keys(), key=(lambda k: distances[k]))
+                out_list.append(key_min)
+                del distances[key_min]
+                ctr += 1
+            seperator = ', '
+            return seperator.join(out_list)
+            #return(key_min)
+        else:
+            return "Dieses Wort befindet sich nicht in der Sammlung."
+
 
 if __name__ == "__main__":
-    find_synonyms("../../../short.cc.de.300.vec", 'Ehefrau', 2)
+    searchType = 'quickSearch'
+    sprache_wordemb = 'd'
+    sprache_wort = 'd'
+    word_orig = 'bla'
+    word_lower = 'bla'
+    anzahl = 3
+    print(find_synonyms(searchType, sprache_wordemb, sprache_wort, word_orig, word_lower, anzahl))
